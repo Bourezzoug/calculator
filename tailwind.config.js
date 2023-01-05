d6fe2d518e78ec4e51e9941f3b5dc097508e1e27
Bourezzoug <bourezzoug.mohamed@gmail.com>
2023-01-05 16:56:13 +0100

    Updated version

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,18 @@ module.exports = {
       "grayish-orange" : 'hsl(28, 16%, 65%)',
       "very-dark-grayish-blue" : 'hsl(221, 14%, 31%)',
       "white" : 'hsl(0, 0%, 100%)',
+      // Theme 2
+      "light-gray-main" : "hsl(0, 0%, 90%)",
+      "grayish-red-keypad" : "hsl(0, 5%, 81%)",
+      "very-light-gray-screen" : "hsl(0, 0%, 93%)",
+      "dark-cyan-bacgkround" : "hsl(185, 42%, 37%)",
+      "dark-cyan-shadow" : "hsl(185, 58%, 25%)",
+      "orange-key-background" : "hsl(25, 98%, 40%)",
+      "orange-key-shadow" : "hsl(25, 99%, 27%)",
+      "grayish-yellow-background" : "hsl(45, 7%, 89%)",
+      "grayish-orange-shadow" : "hsl(35, 11%, 61%)",
+      "yellow-text" : "hsl(60, 10%, 19%)",
+      "white" : "hsl(0, 0%, 100%)",
     }
   },
   plugins: [],
